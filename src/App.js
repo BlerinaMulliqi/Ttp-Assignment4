b7b2a832fd68c9b37a38d50eafab66e61b22b924
Blerina Mulliqi <blerina.mulliqi@lc.cuny.edu>
2019-06-07 11:53:34 -0400

    Tto-Asighnment4

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import  './index.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+//Qestion 1
+class CardContact extends React.Component{
+  render(){
+    return< div className="root">
+          <p>{this.props.name}</p> 
+          <p>{this.props.num}</p> 
+          <p>{this.props.email}</p> </ div>
+  }
+
+  
 }
+function App(){
+  return (
+  <div className= "Cards">
+   <div class= "cards"> <CardContact name='Bina' num="444-444-4444" email="example1@something.com" /></div>
+   <div class= "cards"> <CardContact name='Ina' num="333-333-3333" email="example2@something.com" /></div>
+   <div class= "cards"> <CardContact name='Lona' num="555-555-555" email="example3@something.com" /></div>
+</div>);
+
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
 
 export default App;
